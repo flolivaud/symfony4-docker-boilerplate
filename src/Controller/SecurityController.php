226f7272bf13e00bdb8a6cedfb7606aec794effc
Florent OLIVAUD <olivaud.florent@gmail.com>
2018-09-17 09:17:27 +0200

    Fix security

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -15,9 +15,6 @@ class SecurityController extends AbstractController
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        if ($error) {
-            dump($error);
-        }
 
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
